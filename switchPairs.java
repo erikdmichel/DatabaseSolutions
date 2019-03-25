e4341a07500c8a545ced1026b6d3cafb942233c5
erikdmichel <44000895+erikdmichel@users.noreply.github.com>
2019-03-24 18:24:27 -0700

    Update switchPairs.java

--- switchPairs.java
+++ switchPairs.java
@@ -1,3 +1,8 @@
+//Write a static method switchPairs that switches the order of values in an 
+//ArrayList of Strings in a pairwise fashion. Your method should switch the 
+//order of the first two values, then switch the order of the next two, switch 
+//the order of the next two, and so on.
+
 public class switchPairs  {
    public static void switchPairs(ArrayList<String> list) {
       for (int i = 0; i < list.size() - 1; i+=2) {
