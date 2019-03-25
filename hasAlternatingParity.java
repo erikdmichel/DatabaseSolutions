247ab565531161fc2b2271b846004280f6e0b0b8
erikdmichel <44000895+erikdmichel@users.noreply.github.com>
2019-03-24 18:26:31 -0700

    Update hasAlternatingParity.java

--- hasAlternatingParity.java
+++ hasAlternatingParity.java
@@ -1,3 +1,8 @@
+//Write a static method hasAlternatingParity that returns whether 
+//or not an array of integers has alternating parity (true if it does
+//, false otherwise). The parity of an integer is 0 for even numbers 
+//and 1 for odd numbers.
+
 public class hasAlternatingParity  {
    public static boolean hasAlternatingParity(int[] data) {
       for (int i = 0; i < data.length - 2; i++) {
